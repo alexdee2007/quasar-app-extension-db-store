d0bc39b6848f5aaefa00fcf0ba14903a6b30a3c7
unknown <C:\Users\dag\AppData\Roaming\The Bat!>
2020-06-01 14:36:15 +0300

    refactor for register store module

--- src/index.js
+++ src/index.js
@@ -8,13 +8,19 @@ const path = require('path');
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
+function extendConf (conf) {
+  conf.boot.push('~quasar-app-extension-db-store/src/boot/register-db-store.js');
+  conf.build.transpileDependencies.push(/quasar-app-extension-db-store[\\/]src/)
+}
+
+
 module.exports = function (api) {
 
   api.compatibleWith('quasar', '^1.8.0');
   api.compatibleWith('@quasar/app', '^1.5.0');
 
-  api.extendQuasarConf(conf => conf.build.transpileDependencies.push(/quasar-app-extension-db-store[\\/]src/));
-  api.chainWebpack(chain => chain.resolve.alias.set('db-store$', path.resolve(__dirname, 'db-store.js')));
+  api.extendQuasarConf(extendConf);
+
   api.chainWebpack(chain => chain.resolve.alias.set('db-store', path.resolve(__dirname)));
 
 }
